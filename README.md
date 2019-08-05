110ce2fa2419009b48c1f6cb60255915347410cb
p-doyle <pdoyle003@gmail.com>
2019-08-05 10:17:16 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Micropython-DNSServer-Captive-Portal
+
+This is MicroPython script that creates a Wifi AP with both a DNS and Web Server 
+that act as a 'Captive Portal' by returning its own IP to all DNS queries in
+an attempt to force connecting clients to its Web Server on port 80.  
+When connecting from an Android phone it should automatically prompt you to
+ 'Sign in to the network', which, if clicked, redirects to the Web Server.  If you try
+ browsing to a random domain it *should* redirect you to the web server however
+ if the request was HTTPS you will just get a Connected Refused error. 
+
+The code is based on several other examples of a MicroPython Captive Portal that I found
+on Github however they were 1+ years old and relied on a single loop to accept connection
+for both the DNS and Web servers:
+
+- https://github.com/Matt4/micropython-captive-portal-network-setup
+- https://github.com/amora-labs/micropython-captive-portal
+
+My version uses asyncio to separate the servers and so can be easily extended with
+additional tasks.  Performance and reliability seem better as well.
+
+
+This was tested with an ESP32 using the latest firmware from https://micropython.org/download#esp32.  
+The specific version I tested with was `esp32-20190805-v1.11-187-g00e7fe8ab.bin`.  
+This should work with an ESP8266 and others as well but I have not fully tested it.
+
+# Initial Setup (ESP32 only)
+Since asyncio is not included in the binary for the ESP32 however it must be installed first.  
+Unless you prefer to build your own binary, this requires that your ESP be able to connect 
+to the internet to install.
+-  Open a python terminal with the ESP32 and use the below to connect to your preferred network:
+ ```
+import network
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('yourssid', 'yourpw')
+```
+- Then run:
+```
+import upip
+upip.install('micropython-uasyncio')
+```
+
+# To Do
+I'd like to be able to serve HTTPS as well, which would make this a lot more useful however 
+I was unable to get the Micropython example code working https://github.com/micropython/micropython/blob/master/examples/network/http_server_ssl.py
---
+++ main.py
@@ -0,0 +1,116 @@
+import socket
+import uasyncio as asyncio
+import network
+import gc
+
+
+class DNSQuery:
+	def __init__(self, data):
+		self.data = data
+		self.domain = ''
+		tipo = (data[2] >> 3) & 15  # Opcode bits
+		if tipo == 0:  # Standard query
+			ini = 12
+			lon = data[ini]
+			while lon != 0:
+				self.domain += data[ini + 1:ini + lon + 1].decode('utf-8') + '.'
+				ini += lon + 1
+				lon = data[ini]
+		print("searched domain:" + self.domain)
+
+	def response(self, ip):
+
+		print("Response {} == {}".format(self.domain, ip))
+		if self.domain:
+			packet = self.data[:2] + b'\x81\x80'
+			packet += self.data[4:6] + self.data[4:6] + b'\x00\x00\x00\x00'  # Questions and Answers Counts
+			packet += self.data[12:]  # Original Domain Name Question
+			packet += b'\xC0\x0C'  # Pointer to domain name
+			packet += b'\x00\x01\x00\x01\x00\x00\x00\x3C\x00\x04'  # Response type, ttl and resource data length -> 4 bytes
+			packet += bytes(map(int, ip.split('.')))  # 4bytes of IP
+		print(packet)
+		return packet
+
+# function to handle incoming dns requests
+async def run_dns_server():
+
+    udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # set non-blocking otherwise execution will stop at 'recvfrom' until a connection is received
+    #  and this will prevent the other async threads from running
+    udps.setblocking(False)
+
+    # bind to port 53 on all interfaces
+    udps.bind(('0.0.0.0', 53))
+
+    while True:
+        try:
+            gc.collect()
+
+            data, addr = udps.recvfrom(4096)
+            print("Incoming data...")
+
+            DNS = DNSQuery(data)
+            udps.sendto(DNS.response(SERVER_IP), addr)
+
+            print("Replying: {:s} -> {:s}".format(DNS.domain, SERVER_IP))
+
+            await asyncio.sleep_ms(100)
+
+        except Exception as e:
+            print("Timeout")
+            await asyncio.sleep_ms(3000)
+
+    udps.close()
+
+# function to handle incoming TCP connections
+async def handle_client(reader, writer):
+    try:
+        gc.collect()
+
+        data = await reader.read()
+
+        message = data.decode()
+        addr = writer.get_extra_info('peername')
+        print('Received {} from {}'.format(message, addr))
+
+        if len(message) > 0:
+
+            # load the template and return it to the client
+            with open('index.html') as f:
+                response = f.read()
+
+            yield from writer.awrite(response)
+
+            yield from writer.aclose()
+
+            print("client socket closed")
+
+    except Exception as e:
+        print('failed handling client: {}'.format(e))
+
+    print('done with request from {}'.format(addr))
+
+
+
+print('starting up!')
+
+SERVER_IP = '10.0.0.1'
+SERVER_SUBNET = '255.255.255.0'
+
+# ssid max length is 32 chars
+SERVER_SSID = 'myssid'
+
+# setup the network
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(True)
+ap_if.ifconfig((SERVER_IP, SERVER_SUBNET, SERVER_IP, SERVER_IP))
+ap_if.config(essid=SERVER_SSID, authmode=network.AUTH_OPEN)
+
+
+# create tasks for the web server and the DNS server and then start them
+loop = asyncio.get_event_loop()
+loop.create_task(asyncio.start_server(handle_client, "0.0.0.0", 80))
+loop.create_task(run_dns_server())
+loop.run_forever()
+loop.close()
